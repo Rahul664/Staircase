fb897b8b3147b595964142b79045f2479a38280a
Rahul664 <rahulhoskeri7@gmail.com>
2017-10-05 15:21:24 +0530

    Update staircase.c

--- staircase.c
+++ staircase.c
@@ -1,10 +1,5 @@
 #include <math.h>
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <limits.h>
-#include <stdbool.h>
 
 int main(){
     int a=1,b,c,d,i,r,j,n; 
